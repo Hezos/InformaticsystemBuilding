bac07f9d000e29a5ce7deaa315b91e902c1b0183
Hezos <Nbence0620@gmail.com>
2024-05-04 22:21:38 +0200

    Migrated to angular 17 place component does not work but fresh new components does.

--- Models/index.ts
+++ Models/index.ts
@@ -1,6 +1,16 @@
+import {Guid} from '../FrontendUI/FrontEndUI/node_modules/guid-typescript'
 export interface UserDTO{
     id:number;
     name:string;
     address:string;
     
 };
+
+
+export interface Place
+{
+    id?:Guid;
+    name?:string;
+    address?:string;
+    active?:boolean;
+}
